a6921ec7fd6ebfbe0d962e6aa3a93687561b6809
Ari Gaskins <arigaskins.tech@gmail.com>
2023-02-25 22:11:48 -0500

    light refactoring, comment FIXMEs

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// create arrays for categories
 const canned_items = [
-    'Canned soup/chili', 
-    'Canned fruit', 
-    'Canned veggies', 
-    'Canned tuna/salmon',
-    'Canned meat'
+    "Canned soup/chili", 
+    "Canned fruit", 
+    "Canned veggies", 
+    "Canned tuna/salmon",
+    "Canned meat"
 ]
 
